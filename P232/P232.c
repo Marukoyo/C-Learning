50c31c337f00e1644b5c58d53db0e1d0a7feac02
超级无敌丸子 <z765739302@gmail.com>
2025-03-04 00:30:12 +0800

    Add

---
+++ P232/P232.c
@@ -0,0 +1,106 @@
+/*
+	章节学习视频经常变更，所以不再按照视频标题分P记录
+	214.w 模式 清空
+
+*/
+#include <inttypes.h>  
+#include <math.h>  
+#include <stdbool.h>  
+#include <stdint.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h>  
+#include <windows.h>  
+#include <ctype.h>  
+#include <string.h>  
+#include <signal.h>
+#include <errno.h>
+
+void read_config_safe(const char*);
+void append_log_safe(const char*, const char*);
+void clear_log(const char*);
+int main(void)
+{
+
+	/**
+
+	*/
+	const char* config_filename = "config.txt";
+	const char* log_filename = "log.txt";
+
+	read_config_safe(config_filename);
+
+	const char* str = "select * from students";
+
+	append_log_safe(log_filename, "drop");
+
+	// All other files are closed
+
+	int numclosed = _fcloseall();
+	printf("Number of files closed by _fcloseall(): %d\n", numclosed);
+	clear_log(log_filename);
+	system("pause");
+	return 0;
+}
+void read_config_safe(const char* filename)
+{
+	FILE* file_ptr = NULL;
+	errno_t err = fopen_s(&file_ptr, filename, "r");
+	if (err != 0 || file_ptr == NULL)
+	{
+		char error_msg[256] = { 0 };
+		strerror_s(error_msg, sizeof(error_msg), err);
+		//	是标准错误流（Standard Error），用于输出错误信息或诊断信息。在 C 和 C++ 中，通常使用它来报告程序运行中的错误，而不是将错误信息写入标准输出流 
+		fprintf(stderr, "open file error %s\n", error_msg);
+		exit(EXIT_FAILURE);
+	}
+
+	char buffer[256] = { 0 };
+	while (fgets(buffer, sizeof(buffer), file_ptr) != NULL)
+	{
+		printf("%s", buffer);
+	}
+
+	puts("");
+
+	fclose(file_ptr);
+}
+
+void append_log_safe(const char* filename, const char* msg)
+{
+	FILE* file_ptr = NULL;
+	errno_t err = fopen_s(&file_ptr, filename, "a");
+	if (err != 0 || file_ptr == NULL)
+	{
+		char error_msg[256] = { 0 };
+		strerror_s(error_msg, sizeof(error_msg), err);
+		//	是标准错误流（Standard Error），用于输出错误信息或诊断信息。在 C 和 C++ 中，通常使用它来报告程序运行中的错误，而不是将错误信息写入标准输出流 
+		fprintf(stderr, "Failed to open log file for appending:%s\n", error_msg);
+		exit(EXIT_FAILURE);
+	}
+
+
+	fprintf(file_ptr, "%s\n", msg);
+
+	puts("Append Successfully!");
+
+	fclose(file_ptr);
+}
+
+void clear_log(const char* filename)
+{
+	FILE* file_ptr = NULL;
+	errno_t err = fopen_s(&file_ptr, filename, "w");
+	if (err != 0 || file_ptr == NULL)
+	{
+		char error_msg[256] = { 0 };
+		strerror_s(error_msg, sizeof(error_msg), err);
+		//	是标准错误流（Standard Error），用于输出错误信息或诊断信息。在 C 和 C++ 中，通常使用它来报告程序运行中的错误，而不是将错误信息写入标准输出流 
+		fprintf(stderr, "open file error %s\n", error_msg);
+		exit(EXIT_FAILURE);
+	}
+	// w 模式 会把现有文件的长度截为0；如果文件不存在，则创建一个新文件
+	puts("文件已经打开 w 模式");
+
+	fclose(file_ptr);
+}
